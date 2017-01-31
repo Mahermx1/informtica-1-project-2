dc878a5af454d4a558f7f7bd203fc061a1791a57
Maher <mahermx1@outlook.com>
2017-01-31 12:07:26 +0100

    Add files via upload

---
+++ Images.py
@@ -0,0 +1,11 @@
+import pygame
+
+gameMenu = pygame.image.load("game-menu.jpg")
+regelsnl = pygame.image.load("RegelsNederlands.png")
+regelsen = pygame.image.load("RegelsEngels.png")
+ship_red_big = pygame.image.load("red-big.png")
+ship_red_medium = pygame.image.load("red-medium.png")
+ship_red_small = pygame.image.load("red-small.png")
+ship_blue_big = pygame.image.load("blue-big.png")
+ship_blue_medium = pygame.image.load("blue-medium.png")
+ship_blue_small = pygame.image.load("blue-small.png")
